64950d7d37afcbc6243f1b5d21c7899927f165be
Olivier FAURE <olivier.faure@epitech.eu>
2017-02-02 15:13:06 +0100

    :gear: Rewrite getWidgetValue
    Add GPhotoError symbol
    Rewrite setWidgetValue

--- get_config.js
+++ get_config.js
@@ -2,6 +2,9 @@ var ref = require("ref");
 var gphoto2 = require("./gphoto2");
 var assert = require("assert");
 
+var GPhotoError = Error;
+
+
 function addWidgetToTree(tree, widget) {
     // Code modeled after `display_widgets` in gphoto2/actions.c
     var namePtr = ref.alloc("string");
@@ -11,7 +14,7 @@ function addWidgetToTree(tree, widget) {
     var name = namePtr.deref();
     var label = labelPtr.deref();
 
-    var subtree = getWidgetValue(widget); // TODO - check return val
+    var subtree = getWidgetValue(widget);
 
     var n = gphoto2.gp_widget_count_children(widget);
     for (var i = 0; i < n; ++i) {
@@ -24,113 +27,96 @@ function addWidgetToTree(tree, widget) {
     tree[name || label] = subtree;
 }
 
+var widgetTypeNames = {
+    [gphoto2.GP_WIDGET_TEXT]:       "string",
+    [gphoto2.GP_WIDGET_RANGE]:      "range",
+    [gphoto2.GP_WIDGET_TOGGLE]:     "toggle",
+    [gphoto2.GP_WIDGET_DATE]:       "date",
+    [gphoto2.GP_WIDGET_MENU]:       "choice",
+    [gphoto2.GP_WIDGET_RADIO]:      "choice",
+    [gphoto2.GP_WIDGET_WINDOW]:     "window",
+    [gphoto2.GP_WIDGET_SECTION]:    "section",
+    [gphoto2.GP_WIDGET_BUTTON]:     "button"
+};
+
 function getWidgetValue(widget) {
     var labelPtr = ref.alloc("string");
     var typePtr = ref.alloc("int");
     var ret = gphoto2.GP_OK;
     value = Object();
 
-    gphoto2.gp_widget_get_type(widget, typePtr);
+    gphoto2.gp_widget_get_type(widget, typePtr);    // TODO - check return val
     gphoto2.gp_widget_get_label(widget, labelPtr);
 
     value.label = labelPtr.deref();
+    value.type = widgetTypeNames[typePtr.deref()];
 
-    switch (typePtr.deref())
-    {
-        case gphoto2.GP_WIDGET_TEXT:
-        { /* string */
+    switch (typePtr.deref()) {
+        case gphoto2.GP_WIDGET_TEXT: { /* string */
             var txtPtr = ref.alloc("string");
             ret = gphoto2.gp_widget_get_value(widget, txtPtr);
-
-            if (ret != gphoto2.GP_OK)
-            {
-                throw GphotoError("Failed to retrieve value of text widget " +
-                    value.label);
-            }
-            value.type = "string";
-            value.value = txtPtr.deref();
+            if (ret == gphoto2.GP_OK)
+                value.value = txtPtr.deref();
             break;
         }
 
-        case gphoto2.GP_WIDGET_RANGE:
-        { /* float */
+        case gphoto2.GP_WIDGET_RANGE: { /* float */
             var valuePtr = ref.alloc("float");
             var maxPtr = ref.alloc("float");
             var minPtr = ref.alloc("float");
             var stepPtr = ref.alloc("float");
 
             ret = gphoto2.gp_widget_get_range(widget, minPtr, maxPtr, stepPtr);
-            if (ret != gphoto2.GP_OK)
-            {
-                throw GphotoError("Failed to retrieve values of range widget " +
-                    value.label);
+            if (ret == gphoto2.GP_OK) {
+                ret = gphoto2.gp_widget_get_value(widget, valuePtr);
             }
-            ret = gphoto2.gp_widget_get_value(widget, valuePtr);
-            value =
-            {
-                label: value.label,
-                type: "range",
-                value: valuePtr.deref(),
-                max: maxPtr.deref(),
-                min: minPtr.deref(),
-                step: stepPtr.deref()
-            };
+            if (ret == gphoto2.GP_OK) {
+                value = {
+                    label: value.label,
+                    type: "range",
+                    value: valuePtr.deref(),
+                    max: maxPtr.deref(),
+                    min: minPtr.deref(),
+                    step: stepPtr.deref()
+                };
+            }
             break;
         }
 
-        case gphoto2.GP_WIDGET_TOGGLE:
-        { /* int */
+        case gphoto2.GP_WIDGET_TOGGLE: { /* int */
             var valuePtr = ref.alloc("int");
             ret = gphoto2.gp_widget_get_value(widget, valuePtr);
-            if (ret != gphoto2.GP_OK)
-            {
-                throw GphotoError(
-                    "Failed to retrieve values of toggle widget " +
-                    value.label
-                );
+            if (ret == gphoto2.GP_OK) {
+                value.value = valuePtr.deref();
             }
-            value.type = "toggle";
-            value.value = valuePtr.deref();
             break;
         }
 
-        case gphoto2.GP_WIDGET_DATE:
-        {
+        case gphoto2.GP_WIDGET_DATE: {
             var valuePtr = ref.alloc("int");
             ret = gphoto2.gp_widget_get_value(widget, valuePtr);
-            if (ret != gphoto2.GP_OK)
-            {
-                throw GphotoError(
-                    "Failed to retrieve values of date/time widget " +
-                    value.label
-                );
+            if (ret == gphoto2.GP_OK) {
+                value.value = Date(valuePtr.deref() * 1000.0);
             }
-            value.type = "date";
-            value.value = Date(valuePtr.deref() * 1000.0)
             break;
         }
 
         case gphoto2.GP_WIDGET_MENU:
-        case gphoto2.GP_WIDGET_RADIO:
-        { /* string */
+        case gphoto2.GP_WIDGET_RADIO: { /* string */
             var currentChoicePtr = ref.alloc("string");
             var choicesCount = gphoto2.gp_widget_count_choices(widget);
 
             ret = gphoto2.gp_widget_get_value(widget, currentChoicePtr);
-            if (ret != gphoto2.GP_OK)
-            {
-                throw GphotoError(
-                    "Failed to retrieve value of menu widget " + value.label
-                );
+            if (ret != gphoto2.GP_OK) {
+                break;
             }
             choices = [];
-            for (var i = 0; i < choicesCount; ++i)
-            {
+            for (var i = 0; i < choicesCount; ++i) {
                 var choicePtr = ref.alloc("string");
                 gphoto2.gp_widget_get_choice(widget, i, choicePtr);
+                // TODO - check for errors?
                 choices.push(choicePtr.deref());
             }
-            value.type = "choice";
             value.value = currentChoicePtr.deref();
             value.choices = choices;
             break;
@@ -138,27 +124,25 @@ function getWidgetValue(widget) {
 
         /* ignore: */
         case gphoto2.GP_WIDGET_WINDOW:
-        {
-            value.type = "window";
-            break;
-        }
         case gphoto2.GP_WIDGET_SECTION:
-        {
-            value.type = "section";
+        case gphoto2.GP_WIDGET_BUTTON: {
             break;
         }
-        case gphoto2.GP_WIDGET_BUTTON:
-        {
-            value.type = "button";
-            break;
-        }
-        default:
-        {
-            throw GphotoError(
-                "Failed to retrieve type of widget " + value.label
+
+        default: {
+            throw new GPhotoError(
+                "Retrieved type of widget " + value.label + " is invalid"
             );
         }
     }
+
+    if (ret != gphoto2.GP_OK) {
+        throw new GPhotoError(
+            "Failed to retrieve value of " + value.type
+            + " widget " + value.label
+        );
+    }
+
     return value;
 }
 
@@ -206,8 +190,9 @@ function setWidgetValue(widget, value) {
         );
     }
     else {
-        throw new Error(
-            "Cannot change value of given widget"
+        typeName = widgetTypeNames[typePtr.deref()] || "<unknown type>";
+        throw new GPhotoError(
+            "Cannot change value of given " + typeName + " widget"
         );
     }
 }
